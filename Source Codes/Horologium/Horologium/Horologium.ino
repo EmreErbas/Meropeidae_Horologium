af1f2d922c4c876ce5114bd109820bb5d130857d
Emre Erbas <emre.erbas214@gmail.com>
2023-01-16 10:34:43 +0100

    ESP32-S3 first code trails

--- Source Codes/Horologium/Horologium/Horologium.ino
+++ Source Codes/Horologium/Horologium/Horologium.ino
@@ -12,6 +12,9 @@ Adafruit_IS31FL3731 ledmatrix = Adafruit_IS31FL3731();
 uint8_t sweep[] = {1, 2, 3, 4, 6, 8, 10, 15, 20, 30, 40, 60, 60, 40, 30, 20, 15, 10, 8, 6, 4, 3, 2, 1};
 
 void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
   Serial.begin(9600);
   Serial.println("ISSI swirl test");
 
@@ -21,6 +24,10 @@ void setup() {
   if (! ledmatrix.begin()) {
     Serial.println("IS31 not found");
     while (1);
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(60);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(60);                      // wait for a second
   }
   Serial.println("IS31 found!");
 }
@@ -34,10 +41,14 @@ is the PWM of the LED, 0 is off, and 255 is all the way on.
 
 This loop will light up every LED in increasing brightness: */
 void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(200);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   // animate over all the pixels, and set the brightness from the sweep table
   for (uint8_t incr = 0; incr < 24; incr++)
     for (uint8_t x = 0; x < 16; x++)
       for (uint8_t y = 0; y < 9; y++)
         ledmatrix.drawPixel(x, y, sweep[(x+y+incr)%24]);
-  delay(20);
+  delay(200);
+   Serial.println("IS31 not found");
 }
