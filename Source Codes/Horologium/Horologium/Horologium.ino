988a10ed83c8f920a84e737570873f8853d3ac49
Emre Erbas <emre.erbas214@gmail.com>
2023-01-20 20:57:49 +0100

    working version of 2 individual clk on single I2C

--- Source Codes/Horologium/Horologium/Horologium.ino
+++ Source Codes/Horologium/Horologium/Horologium.ino
@@ -3,13 +3,13 @@
 #include <Adafruit_IS31FL3731.h>
 
 // If you're using the full breakout...
-Adafruit_IS31FL3731 ledmatrix = Adafruit_IS31FL3731();
+Adafruit_IS31FL3731 ledmatrix1 = Adafruit_IS31FL3731();
 Adafruit_IS31FL3731 ledmatrix2 = Adafruit_IS31FL3731();
 Adafruit_IS31FL3731 ledmatrix3 = Adafruit_IS31FL3731();
 Adafruit_IS31FL3731 ledmatrix4 = Adafruit_IS31FL3731();
 
-TwoWire DefaultLine = TwoWire();
-TwoWire AlteredLine = TwoWire();
+TwoWire DefaultLine = TwoWire(0);
+TwoWire AlteredLine = TwoWire(1);
 
 // The lookup table to make the brightness changes be more visible
 uint8_t sweep[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 29, 32, 35, 38, 41, 44,
@@ -27,23 +27,27 @@ void setup() {
   DefaultLine.setPins(3, 4);
   AlteredLine.setPins(3, 5);
   
+  ledmatrix1.begin(0x74, &DefaultLine);
+  ledmatrix2.begin(0x77, &DefaultLine);
+  ledmatrix1.hardClear();
+  ledmatrix2.hardClear();
+  ledmatrix1.end();
+  ledmatrix2.end(); 
 
+  ledmatrix3.begin(0x74, &AlteredLine);  
+  ledmatrix4.begin(0x77, &AlteredLine);
+  ledmatrix3.hardClear();
+  ledmatrix4.hardClear();
+  ledmatrix3.end();
+  ledmatrix4.end(); 
 
 // call begin(address) to initialize the driver. 
 // Begin() will return false if the matrix was not found,
 //                 and true if initialization worked out
-  if (! ledmatrix.begin()) {
-    Serial.println("IS31 not found");
-    while (1);
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(60);                      // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-    delay(60);                      // wait for a second
-  }
-  Serial.println("IS31 found!");
-  ledmatrix2.begin(0x75);
-  ledmatrix3.begin(0x76);  
-  ledmatrix4.begin(0x77);
+//  ledmatrix1.begin(0x74, &DefaultLine);
+//  ledmatrix2.begin(0x75, &DefaultLine);
+//  ledmatrix3.begin(0x76, &AlteredLine);  
+//  ledmatrix4.begin(0x77, &AlteredLine);
 }
 /* Drawing
 You can then draw to the display. Note that since we write directly 
@@ -60,18 +64,54 @@ void loop() {
 
   // activate the default clock line to reach the upper drivers
 
+  ledmatrix1.begin(0x74, &DefaultLine);
+  ledmatrix2.begin(0x77, &DefaultLine);
   for (uint8_t incr = 0; incr < 48; incr+=2)
   {
     for (uint8_t x = 0; x < 16; x++)
     {
       for (uint8_t y = 0; y < 9; y++)
       {
-        ledmatrix.setLEDPWM (x + (y << 4), sweep[(x + y + incr + 2) % 48]);
+        ledmatrix1.setLEDPWM(x + (y << 4), sweep[(x + y + incr + 2) % 48]);
+      }
+    }
+  }
+  for (uint8_t incr = 0; incr < 48; incr+=2)
+  {
+    for (uint8_t x = 0; x < 16; x++)
+    {
+      for (uint8_t y = 0; y < 9; y++)
+      {
         ledmatrix2.setLEDPWM(x + (y << 4), sweep[(x + y + incr) % 48]);    
-        ledmatrix3.setLEDPWM(x + (y << 4), sweep[(x + y + incr) % 48]); 
+      }
+    }
+  }
+  ledmatrix1.end();
+  ledmatrix2.end();
+
+  ledmatrix3.begin(0x74, &AlteredLine);  
+  ledmatrix4.begin(0x77, &AlteredLine);
+  for (uint8_t incr = 0; incr < 48; incr+=2)
+  {
+    for (uint8_t x = 0; x < 16; x++)
+    {
+      for (uint8_t y = 0; y < 9; y++)
+      {  
+        ledmatrix3.setLEDPWM(x + (y << 4), sweep[(x + y + incr + 2) % 48]); 
+      }
+    }
+  }
+  for (uint8_t incr = 0; incr < 48; incr+=2)
+  {
+    for (uint8_t x = 0; x < 16; x++)
+    {
+      for (uint8_t y = 0; y < 9; y++)
+      {  
         ledmatrix4.setLEDPWM(x + (y << 4), sweep[(x + y + incr) % 48]); 
       }
     }
   }
+  ledmatrix3.end();
+  ledmatrix4.end();
   
 }
